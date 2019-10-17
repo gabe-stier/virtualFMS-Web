a866a32a8be3393e3f1c259f44654e29b62cb0f5
Gabe <gabezter4@yahoo.com>
2019-10-16 20:31:55 -0500

    Added gitignore and extra files

--- indexVFMS.php
+++ indexVFMS.php
@@ -12,38 +12,53 @@ function files($loc){
 }
 ?>
 
-<html><head>
-	<title>Virtual File Management System</title>
-	<link rel="stylesheet" href="styles/indexVFMS.css">
-	<script type="text/javascript">
-		function openLoginForm() {
-			document.getElementById("loginForm").style.display = "block";
-		}
+<html>
 
-		function closeLoginForm() {
-			document.getElementById("loginForm").style.display = "none";
-		}
-		
-	</script>
+<head>
+    <title>Virtual File Management System</title>
+    <link rel="stylesheet" href="styles/indexVFMS.css">
+    <script type="text/javascript">
+        function openLoginForm() {
+            document.getElementById("loginForm").style.display = "block";
+        }
+
+        function closeLoginForm() {
+            document.getElementById("loginForm").style.display = "none";
+        }
+        var toggler = document.getElementsByClassName("caret");
+        var i;
+
+        for (i = 0; i < toggler.length; i++) {
+            toggler[i].addEventListener("click", function() {
+                this.parentElement.querySelector(".nested").classList.toggle("active");
+                this.classList.toggle("caret-down");
+            });
+        }
+
+    </script>
 </head>
 <header>
-	<h1>Basher's Group</h1><button onclick="openLoginForm()" href="indexVFMS.js">Login</button><button>Sign Up</button><button style="display: none">Log Out</button><button>Calculator</button>
+    <h1>Basher's Group</h1><button onclick="openLoginForm()" href="indexVFMS.js">Login</button><button>Sign Up</button><button style="display: none">Log Out</button><button>Calculator</button>
 </header>
 
 <body>
-	<div class="form-popup" id="loginForm">
-		<form action="/action_page.php" class="form-container">
-			<h1>Login</h1>
+    <div class="form-popup" id="loginForm">
+        <form action="/action_page.php" class="form-container">
+            <h1>Login</h1>
 
-			<label for="email"><b>Username</b></label>
-			<input type="text" placeholder="Enter Username" name="username" required>
+            <label for="email"><b>Username</b></label>
+            <input type="text" placeholder="Enter Username" name="username" required>
 
-			<label for="pwd"><b>Password</b></label>
-			<input type="password" placeholder="Enter Password" name="pwd" required>
+            <label for="pwd"><b>Password</b></label>
+            <input type="password" placeholder="Enter Password" name="pwd" required>
 
-			<button type="submit" class="btn">Login</button>
-			<button type="submit" class="btn cancel" onclick="closeLoginForm()">Close</button>
-		</form>
-	</div>
-	<div title="Files" class="fileViewer"><ul id="treeFile"><?= files($dir); ?></ul></div>
-</body></html>
+            <button type="submit" class="btn">Login</button>
+            <button type="submit" class="btn cancel" onclick="closeLoginForm()">Close</button>
+        </form>
+    </div>
+    <div title="Files" class="fileViewer">
+        <ul id="treeFile" onclick="labels()"><?= files($dir); ?></ul>
+    </div>
+</body>
+
+</html>
