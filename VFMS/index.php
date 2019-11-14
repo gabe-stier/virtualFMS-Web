1a3b8688264eea64484e2579fd4e31b2d492b294
Gabe Stier <37159135+gabe-stier@users.noreply.github.com>
2019-11-14 10:44:44 -0600

    Beautified Code

--- VFMS/index.php
+++ VFMS/index.php
@@ -1,25 +1,25 @@
-﻿<?php
-session_start();
-$dir = '/var/protected';
-$me = "<script>alert('test')</script>";
-function files($loc) {
-    $me = "<script>alert(\'test\')</script>";
-    foreach (new DirectoryIterator($loc) as $file) if ($file->isFile()) {
-        print "<li>" . $file->getFilename() . "\t" . "<form action='download.php' method='post'><input type='hidden' name='filename' value='" . $file->getFilename() . "'><input type='hidden' name='file' value='" . $file->getPathname() . "'><button class='btn btn-primary' type='submit'>Download</button></form> " .
-        "</li>\n";
-    } else if ($file != '..' && $file != '.') {
-        print "<li><span class=\"caret\">" . $file->getFilename() . "</span><ul class=\"nested\">";
-        files($file->getPathname());
-        print "</ul></li>";
-    }
-}
-function testSessionId() {
-    if (isset($_SESSION['sesID'])) {
-        return 1;
-    } else {
-        return 0;
-    }
-}
+<?php
+	session_start();
+	$dir = '/var/protected';
+	$me = "<script>alert('test')</script>";
+	function files($loc) {
+	    $me = "<script>alert(\'test\')</script>";
+	    foreach (new DirectoryIterator($loc) as $file) if ($file->isFile()) {
+		print "<li>" . $file->getFilename() . "\t" . "<form action='download.php' method='post'><input type='hidden' name='filename' value='" . $file->getFilename() . "'><input type='hidden' name='file' value='" . $file->getPathname() . "'><button class='btn btn-primary' type='submit'>Download</button></form> " .
+		"</li>\n";
+	    } else if ($file != '..' && $file != '.') {
+		print "<li><span class=\"caret\">" . $file->getFilename() . "</span><ul class=\"nested\">";
+		files($file->getPathname());
+		print "</ul></li>";
+	    }
+	}
+	function testSessionId() {
+	    if (isset($_SESSION['sesID'])) {
+		return 1;
+	    } else {
+		return 0;
+	    }
+	}
 ?>
 
 <html>
@@ -63,17 +63,20 @@ function testSessionId() {
 		<hr>
 		<div class="row justify-content-center">
 			<?php
-if (testSessionId() == 0) { ?>
-			<div class="col-sm-2">
-				<button class="btn btn-success" onclick="openLoginWindow()">Login</button></div>
-			<div class="col-sm-2"><button class="btn btn-secondary" onclick="openSignUpWindow()">Sign Up</button></div><?php
-} else { ?> <button class="btn btn-danger" onclick="window.location.href='logout.php'">Logout</button>
-
+				if (testSessionId() == 0) { ?>
+					<div class="col-sm-2">
+					<button class="btn btn-success" onclick="openLoginWindow()">Login</button></div>
+					<div class="col-sm-2"><button class="btn btn-secondary" onclick="openSignUpWindow()">Sign Up</button></div><?php
+				} else {
+			?>
+					<button class="btn btn-danger" onclick="window.location.href='logout.php'">Logout</button>
 			<?php
-}
-?>
+				}
+			?>
+			
 			<div class="col-sm-2"><button class="btn btn-dark" onclick="window.location.href='../Calculator/'">Calculator</button></div>
-            <div class="col-sm-2"><button class="btn btn-warning" onclick="window.location.href='../Lights/'">Light Controller</button></div>
+            		<div class="col-sm-2"><button class="btn btn-warning" onclick="window.location.href='../Lights/'">Light Controller</button></div>
+		
 		</div>
 	</div>
 	<hr>
@@ -81,15 +84,18 @@ if (testSessionId() == 0) { ?>
 
 <body>
 	<div title="Files">
-		<?php if (testSessionId() != 0) { ?>
-		<ul id="treeFile" onclick="labels()">
-			<?=files($dir); ?>
-		</ul>
+		<?php 
+			if (testSessionId() != 0) { ?>
+				<ul id="treeFile" onclick="labels()">
+					<?=files($dir); ?>
+				</ul>
 		<?php
-} else { ?>
-		<p> You can not access the file system while you are not logged in! </p>
+			} else {
+		?>
+			<p> You can not access the file system while you are not logged in! </p>
 		<?php
-} ?></div>
+			}
+		?>
+	</div>
 </body>
-
 </html>
