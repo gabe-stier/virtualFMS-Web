10f98dac7a35e2ca965f092d8775f24fef9a8092
Gabe Stier <37159135+gabe-stier@users.noreply.github.com>
2019-11-14 10:31:17 -0600

    Added iFrame code

--- Lights/index.php
+++ Lights/index.php
@@ -7,8 +7,7 @@ $slev = "getGroup";
 //function files($loc) {
 //    $me = "<script>alert(\'test\')</script>";
 //    foreach (new DirectoryIterator($loc) as $file) if ($file->isFile()) {
-//        print "<li>" . $file->getFilename() . "\t" . "<form action='download.php' method='post'><input type='hidden' name='filename' value='" . $file->getFilename() . "'><input type='hidden' name='file' value='" . $file->getPathname() . "'><button class='btn btn-primary' type='submit'>Download</button></form> " .
-//        "</li>\n";
+//        print "<li>" . $file->getFilename() . "\t" . "<form action='download.php' method='post'><input type='hidde$//        "</li>\n";
 //    } else if ($file != '..' && $file != '.') {
 //        print "<li><span class=\"caret\">" . $file->getFilename() . "</span><ul class=\"nested\">";
 //        files($file->getPathname());
@@ -27,35 +26,36 @@ function testSessionId() {
 <html>
 
 <head>
-	<title>Light Controller</title>
-	<link rel="stylesheet" href="../VFMS/bootstrap-4.3.1-dist/css/bootstrap.min.css">
-    
-	<script type="text/javascript">
-//		function readFile() {
-//			window.open('../VFMS/readfile.php', '_blank', 'height=400,width=400');
-//		}
-//
-//		function openLoginWindow() {
-//			window.open('../VFMS/login.html', '_blank', 'height=400,width=400');
-//		}
-//
-//		function openSignUpWindow() {
-//			window.open('../VFMS/signup.html', '_blank', 'height=400,width=400');
-//		}
-//		var toggler = document.getElementsByClassName("caret");
-//		var i;
+        <title>Light Controller</title>
+        <link rel="stylesheet" href="../VFMS/bootstrap-4.3.1-dist/css/bootstrap.min.css">
 
-		function labels() {
-			for (i = 0; i < toggler.length; i++) {
-				toggler[i].addEventListener("click", function() {
-					this.parentElement.querySelector(".nested").classList.toggle("active");
-					this.classList.toggle("caret-down");
-				});
-			}
-		}
+        <script type="text/javascript">
+//              function readFile() {
+//                      window.open('../VFMS/readfile.php', '_blank', 'height=400,width=400');
+//              }
+//
+//              function openLoginWindow() {
+//                      window.open('../VFMS/login.html', '_blank', 'height=400,width=400');
+//              }
+//
+//              function openSignUpWindow() {
+//                      window.open('../VFMS/signup.html', '_blank', 'height=400,width=400');
+//              }
+//              var toggler = document.getElementsByClassName("caret");
+//              var i;
+
+                function labels() {
+                        for (i = 0; i < toggler.length; i++) {
+                                toggler[i].addEventListener("click", function() {
+                                        this.parentElement.querySelector(".nested").classList.toggle("active");
+                                        this.classList.toggle("caret-down");
+                                });
+                        }
+                }
         function buttons(slev) {
             if ($slev == "AdmiN TesT")
                 {
+               
                     // Show all buttons
                 }
             else if ($slev == "System Admin")
@@ -68,52 +68,51 @@ function testSessionId() {
                 }
         }
 
-	</script>
+        </script>
 </head>
 <header>
-	<div class="container vertical-center">
+        <div class="container vertical-center">
 
-		<center>
-			<h1>Light Change</h1>
-		</center>
-		<br>
+                <center>
+                        <h1>Light Change</h1>
+                </center>
+                <br>
             <h3 style="padding-top: 100px;padding-left: 100px;">Your Privilege Level: <?php echo $slev ?></h3>
-        
-		<div class="row justify-content-center">
-			<?php
+
+                <div class="row justify-content-center">
+                        <?php
 if (testSessionId() == 0) { ?>
-			<div class="col-sm-2">
-				<button class="btn btn-success" onclick="openLoginWindow()">Login</button></div>
-			<div class="col-sm-2"><button class="btn btn-secondary" onclick="openSignUpWindow()">Sign Up</button></div><?php
+                        <div class="col-sm-2">
+                                <button class="btn btn-success" onclick="openLoginWindow()">Login</button></div>
+                        <div class="col-sm-2"><button class="btn btn-secondary" onclick="openSignUpWindow()">Sign Up$
 } else { ?> <button class="btn btn-danger" onclick="window.location.href='../VFMS/logout.php'">Logout</button>
 
-			<?php
+                        <?php
 }
 ?>
-			<div class="col-sm-2"><button class="btn btn-dark" onclick="window.location.href='../Calculator/'">Calculator</button></div>
-            <div class="col-sm-2"><button class="btn btn-warning" onclick="window.location.href='../VFMS/'">File System</button></div>
-		</div>
-	</div>
-	<hr>
+                        <div class="col-sm-2"><button class="btn btn-dark" onclick="window.location.href='../Calcula$            <div class="col-sm-2"><button class="btn btn-warning" onclick="window.location.href='../VFMS/'">File Sys$                </div>
+        </div>
+        <hr>
 </header>
 
 <body>
-	<div title="Light Buttons" class="row justify-content-center">
-		<?php if (testSessionId() != 0) { ?>
-		<ul id="treeFile" onclick="labels()">
-			<?=files($dir); ?>
-		</ul>
-		<?php
+        <div title="Light Buttons" class="row justify-content-center">
+                <?php if (testSessionId() != 0) { ?>
+                <ul id="treeFile" onclick="labels()">
+                        <?=files($dir); ?>
+                </ul>
+                <?php
 } else { ?>
-		<p> You can not access the light controller while you are not logged in! </p>
-		<?php
+                <p> You can not access the light controller while you are not logged in! </p>
+                <?php
 } ?></div>
 <!-- <form action="http://192.168.0.19" method="get"> -->
 <!-- <form action="http://192.168.137.178 method="get"> -->
-	<!-- <form target="lights" method="get"> -->
+         <form target="lights" method="get">
     <div class="row justify-content-center">
 
             
+
             <div class="col-sm-1"><label>Red: </label>
                 <input type="checkbox" name="red" id="red"></div>
             <div class="col-sm-1"><label>Blue: </label>
@@ -121,21 +120,38 @@ if (testSessionId() == 0) { ?>
             <div class="col-sm-1"><label>Green: </label>
                 <input type="checkbox" name="green" id="green"></div><br>
 
-        
+
     </div>
             <center>
-		<!--    <input class="btn btn-secondary" type="submit" placeholder="Submit"> -->
-		<!-- 	<input class="btn btn-secondary" type="submit" placeholder="Submit" onclick="setIframeSrc(); return false;"> -->
-		    <script>  
-			    function setIframeSrc(){
-				    var red = document.getElementById('red').value;
-				    var blue = document.getElementById('blue').value;
-				    var green = document.getElementById('green').value;
-				    alert('red -> ' + red + '\n blue -> ' + blue + '\n green -> ' + green);
-			    }
-		    </script>
-	</center>
-            </form>
+                <!--    <input class="btn btn-secondary" type="submit" placeholder="Submit"> -->
+                        <input class="btn btn-secondary" type="submit" placeholder="Submit" onclick="setIframeSrc();$
+                    <script>
+                            function setIframeSrc(){
+                                    if( document.getElementById('red').checked)
+                                        var red = 'on';
+                                    else
+                                        var red = 'off';
+
+                                    if( document.getElementById('blue').checked)
+                                        var blue = 'on';
+                                    else
+                                        var blue = 'off';
+
+                                    if( document.getElementById('green').checked)
+                                        var green = 'on';
+                                    else
+                                        var green = 'off';
+												     
+												     
+//                                      document.getElementById('red').value = red;
+//                                      document.getElementById('blue').value = blue;
+//                                      document.getElementById('green').value = green;
+//                                  alert('red -> ' + red + '\nblue -> ' + blue + '\ngreen -> ' + green);
+//                              alert(document.getElementById('red').value + '\n' + document.getElementById('blue').$                                    document.getElementById('lights').src ='http://192.168.137.145/?red='+red+'&blue$//                              document.getElementById('iframe').location = 'http
+                            }
+                    </script>
+        </center></form>
+<iframe name='lights' id='lights' src='http://192.168.137.145/' style='border: none;'></iframe>
 </body>
 
 </html>
