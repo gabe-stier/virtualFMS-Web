ee5b44df18992eb2bd9928b138dbfb0c34411f6a
Gabe Stier <gabezter4@yahoo.com>
2019-11-19 17:45:30 +0000

    Created basic script to upload file. NEEDS to be tested.

--- VFMS/upload.php
+++ VFMS/upload.php
@@ -1,6 +1,24 @@
 <?php
 $dir = "/var/protected/";
 $file = $dir.basename($_FILES['fileToUpload']['name']);
+$tempFile = $file;
+function upload(){
+	if(isset($_POST['submit'])) {
+		fileName(0);
+		if(move_uploaded_file($_FILES["fileToUpload"]["tmp_name"],$file){
+			echo "File has been uploaded";
+		}else{
+			echo "Could not upload the file";
+		}
+	}
+}
+function fileName($num){
+	$num += 1;
+	if(file_exists($file)){
+		$file = $tempFile. "(".$num.")";
+		return fileName($num);
+	}
+}
 
-if(isset($_POST['submit'])) {
-	if(move)
+upload();
+?>
