c289b04bc32bb55cc170aa38849a4ffed8d24131
Gabe Stier <gabezter4@yahoo.com>
2019-11-19 18:17:00 +0000

    upload.html -> Changed text shown on a button. upload.php -> Tested and fixed bugs in file upload.

--- VFMS/upload.php
+++ VFMS/upload.php
@@ -1,23 +1,28 @@
 <?php
 $dir = "/var/protected/";
 $file = $dir.basename($_FILES['fileToUpload']['name']);
+var_dump($_FILES);
+echo $file;
 $tempFile = $file;
 function upload(){
 	if(isset($_POST['submit'])) {
-		fileName(0);
-		if(move_uploaded_file($_FILES["fileToUpload"]["tmp_name"],$file){
-			echo "File has been uploaded";
+		$tempfile =	fileName(0, "/var/protected/".basename($_FILES["fileToUpload"]["name"]));
+		if(move_uploaded_file($_FILES["fileToUpload"]["tmp_name"],$tempfile)){
+//			print "File has been uploaded";
+			echo "<script>window.close();</script>";
 		}else{
-			echo "Could not upload the file";
+			print "Could not upload the file";
+			echo "<script> setTimeout(function(){ window.close(); }, 2000); </script>";
 		}
 	}
 }
-function fileName($num){
+function fileName($num, $lFile){
 	$num += 1;
-	if(file_exists($file)){
-		$file = $tempFile. "(".$num.")";
-		return fileName($num);
+	if(file_exists($lFile)){
+		$lFile = "/var/protected/"."(".$num.")_".basename($_FILES['fileToUpload']['name']);
+		return fileName($num, $lFile);
 	}
+	return $lFile;
 }
 
 upload();
